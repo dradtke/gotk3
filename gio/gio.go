bc2c637bc818d54a16f492371f5e22dff38b07d9
Damien Radtke <damienradtke@gmail.com>
2013-09-20 02:18:31 -0500

    More GApplication support.

--- gio/gio.go
+++ gio/gio.go
@@ -74,6 +74,15 @@ func wrapApplication(obj *glib.Object) Application {
 	return Application{*obj}
 }
 
+// Native() returns a pointer to the underlying GApplication.
+func (v *Application) Native() *C.GApplication {
+	if v == nil || v.Ptr() == nil {
+		return nil
+	}
+	return (*C.GApplication)(v.Ptr())
+}
+
+// ApplicationNew() is a wrapper around g_application_new().
 func ApplicationNew(id string, flags ApplicationFlags) (*Application, error) {
 	cstr := C.CString(id)
 	defer C.free(unsafe.Pointer(cstr))
@@ -86,3 +95,84 @@ func ApplicationNew(id string, flags ApplicationFlags) (*Application, error) {
 	runtime.SetFinalizer(obj, (*glib.Object).Unref)
 	return &a, nil
 }
+
+// ApplicationId() is a wrapper around g_application_get_application_id().
+func (v *Application) ApplicationId() string {
+	c := C.g_application_get_application_id(v.Native())
+	return C.GoString((*C.char)(c))
+}
+
+// SetApplicationId() is a wrapper around g_application_set_application_id().
+func (v *Application) SetApplicationId(id string) {
+	cstr := C.CString(id)
+	defer C.free(unsafe.Pointer(cstr))
+	C.g_application_set_application_id(v.Native(), (*C.gchar)(cstr))
+}
+
+// InactivityTimeout() is a wrapper around g_application_get_inactivity_timeout().
+func (v *Application) InactivityTimeout() uint {
+	c := C.g_application_get_inactivity_timeout(v.Native())
+	return uint(c)
+}
+
+// SetInactivityTimeout() is a wrapper around g_application_set_inactivity_timeout().
+func (v *Application) SetInactivityTimeout(timeout uint) {
+	C.g_application_set_inactivity_timeout(v.Native(), C.guint(timeout))
+}
+
+// Flags() is a wrapper around g_application_get_flags().
+func (v *Application) Flags() ApplicationFlags {
+	c := C.g_application_get_flags(v.Native())
+	return ApplicationFlags(c)
+}
+
+// SetFlags() is a wrapper around g_application_set_flags().
+func (v *Application) SetFlags(flags ApplicationFlags) {
+	C.g_application_set_flags(v.Native(), C.GApplicationFlags(flags))
+}
+
+// Hold() is a wrapper around g_application_hold().
+func (v *Application) Hold() {
+	C.g_application_hold(v.Native())
+}
+
+// Release() is a wrapper around g_application_release().
+func (v *Application) Release() {
+	C.g_application_release(v.Native())
+}
+
+func (v *Application) Quit() {
+	C.g_application_quit(v.Native())
+}
+
+// Run() is a wrapper around g_application_run().
+func (v *Application) Run(args []string) int {
+	var c C.int
+	if args != nil {
+		argc := len(args)
+		argv := make([]*C.char, argc)
+		for i, arg := range args {
+			argv[i] = C.CString(arg)
+		}
+		c = C.g_application_run(v.Native(), C.int(argc),
+			(**C.char)(unsafe.Pointer(&argv)))
+	} else {
+		c = C.g_application_run(v.Native(), 0, nil)
+	}
+	return int(c)
+}
+
+// Need at least GIO 2.38
+/*
+// MarkBusy() is a wrapper around g_application_mark_busy().
+func (v *Application) MarkBusy() {
+	C.g_application_mark_busy(v.Native())
+}
+
+// UnmarkBusy() is a wrapper around g_application_unmark_busy().
+func (v *Application) UnmarkBusy() {
+	C.g_application_unmark_busy(v.Native())
+}
+*/
+
+// TODO: add GDBusConnection
